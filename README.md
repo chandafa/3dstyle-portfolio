ee06ea95aa8c44ec885e557669c227a3ab3751a6
chandafa <chankirana722@gmail.com>
2023-07-27 08:56:01 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Get started!
+
+## First
+
+Start by `forking` this repository, then clone your forked copy and navigate to the root directory.
+
+## Install the dependencies with: 
+
+### `yarn`
+
+## Then start the development server with:
+
+### `yarn start`
+
+This runs the app in the development mode.
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+
+## Deploy to github pages
+
+First make sure to create a `gh-pages` branch.
+
+After pushing the branch to GitHub, head over to the `Settings` page of the repository and select in the `GitHub Pages` section the `gh-pages` branch as source.
+
+After that run:
+
+### `yarn deploy`
+
+And thats It! It will automatically deploy to `https://{username}.github.io/{repo-name}`
+
---
+++ src/configs.js
@@ -0,0 +1,154 @@
+import {
+  displayAt,
+  STOPS,
+  PAGE_DEPH_MARGIN,
+  SKILLS_LIST,
+  EXPERIENCE,
+} from './constants/pageData';
+
+const configs = {
+  title: 'Candra Kirana',
+  subTitle: 'Web and App Dev.',
+  stars: {
+    maxSize: 20,
+    emissionIntencity: 40,
+    //optionally stars can be an image
+    // url: 'icons/three-js-logo.png',
+    dencity: 900,
+  },
+  navTitle: 'Candra Kirana | My Portfolio.',
+  relevantStops: STOPS,
+  pages: [
+    {
+      scale: 15,
+      rotation: [Math.PI, 0, Math.PI],
+      position: [0, 0, -PAGE_DEPH_MARGIN],
+      displayAt: displayAt('About'),
+      title: 'Siapakah saya?',
+      infos: [
+        "* Saya adalah mahasiswa Universitas Pasunda (UNPAS) Fakultas Teknik, Jurusan Teknik Informatika.",
+        '* My journey as a software developer started when I was a just a kid trying to figure out how video games where made.',
+        '* I started my career as a front-end developer around 3 years ago in 2020, and since then I have helped many startup companies and non-profit organizations to develop quality products.',
+        '* I have a wide range of skills, of which I can say that I’ve mastered many.',
+      ],
+    },
+    {
+      scale: 15,
+      displayAt: displayAt('Experience'),
+      rotation: [0, -Math.PI / 2, 0],
+      position: [-PAGE_DEPH_MARGIN, 0, 0],
+      title: 'Experience',
+      imageList: EXPERIENCE,
+      footer: 'You can click the icons to find out more.',
+    },
+    {
+      scale: 15,
+      displayAt: displayAt('Skills'),
+      rotation: [-Math.PI / 2, 0, -Math.PI / 2],
+      position: [0, PAGE_DEPH_MARGIN, 0],
+      title: 'Skills',
+      footer: 'And more...',
+      imageList: SKILLS_LIST,
+    },
+    {
+      scale: 15,
+      displayAt: displayAt('Download CV'),
+      rotation: [0, 0, 0],
+      position: [0, 0, PAGE_DEPH_MARGIN],
+      title: 'Here is my CV!',
+      footer: 'Click the icon above to view!',
+      imageList: {
+        rows: 1,
+        separation: 3,
+        leftPadding: 0,
+        topPadding: 0,
+        scale: 0.8,
+        items: [
+          {
+            url: 'icons/resume.png',
+            websiteURL:
+              'https://drive.google.com/file/d/1pYKvta_rm-QvBki_FgGgcJv0OYJMjRiX/view?usp=drive_link',
+          },
+        ],
+      },
+    },
+    {
+      scale: 15,
+      displayAt: displayAt('Personal Projects'),
+      rotation: [Math.PI / 2, 0, 0],
+      position: [0, -PAGE_DEPH_MARGIN, 0],
+      title: 'Personal Projects',
+      footer: '(And also this web-app)',
+      imageList: {
+        rows: 2,
+        separation: 2.5,
+        leftPadding: -0.4,
+        topPadding: 0.2,
+        scale: 0.3,
+        items: [
+          {
+            url: 'websites-preview/nft-place-logo.png',
+            websiteURL: 'https://nft-place.xyz/',
+          },
+          {
+            url: 'websites-preview/snow-hero.png',
+            websiteURL: 'https://powtom48.itch.io/snow-the-hero',
+          },
+          {
+            url: 'websites-preview/minecraft-in-js.png',
+            websiteURL:
+              'https://www.youtube.com/watch?v=yOUQquusyiY&t=123s&ab_channel=tomugon',
+          },
+          {
+            url: 'websites-preview/fish-chopter.png',
+            websiteURL: 'https://powtom48.itch.io/fishcopter',
+          },
+          {
+            url: 'websites-preview/odd-clowns.png',
+            websiteURL: 'https://oddclowns.com/',
+          },
+          {
+            url: 'websites-preview/island-ai.png',
+            websiteURL: 'https://www.instagram.com/island.ai/',
+          },
+        ],
+      },
+    },
+    {
+      scale: 15,
+      displayAt: displayAt('Get In Touch'),
+      rotation: [0, Math.PI / 2, 0],
+      position: [PAGE_DEPH_MARGIN, 0, 0],
+      title: 'You can find me in:',
+      footer: 'Almost done but keep scrolling :)',
+      imageList: {
+        rows: 2,
+        separation: 1.8,
+        leftPadding: -0.25,
+        topPadding: 0.2,
+        scale: 0.3,
+        items: [
+          {
+            url: 'icons/twitter.png',
+            websiteURL: 'https://twitter.com/chann',
+          },
+          {
+            url: 'icons/linkedin.png',
+            websiteURL:
+              'https://www.linkedin.com/in/chandra-kirana-00b835244',
+          },
+          {
+            url: 'icons/github.png',
+            websiteURL: 'https://github.com/chandafa',
+          },
+          {
+            url: 'icons/email.png',
+            websiteURL: 'mailto:ck271138@gmail.com',
+          },
+        ],
+      },
+    },
+  ],
+};
+
+export default configs;
